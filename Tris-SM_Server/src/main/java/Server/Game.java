c07ca478dcea3679a90186abbc9f623e88139734
Elisei <mailatelisei27@gmail.com>
2026-01-15 12:11:22 +0100

    Added functions of the Game

--- Tris-SM_Server/src/main/java/Server/Game.java
+++ Tris-SM_Server/src/main/java/Server/Game.java
@@ -1,5 +1,41 @@
 package Server;
 
-public class Game {
 
+public class Game {
+    private char[][] board;
+    private char currentPlayer;
+    private boolean gameActive;
+    private String winner;
+
+    public Game() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) board[i][j] = '-';
+        }
+        currentPlayer = 'X';
+        gameActive = true;
+        winner = null;
+    }
+    
+    public synchronized boolean makeMove(int row, int col) {
+        
+    }
+
+    private void switchPlayer() {
+        
+    }
+
+    private boolean checkWin(int r, int c) {
+        
+    }
+
+    private boolean isBoardFull() {
+        
+    }
+
+    // Getters per il Server
+    public char getCurrentPlayer() { return currentPlayer; }
+    public boolean isGameActive() { return gameActive; }
+    public String getWinner() { return winner; }
+    public char[][] getBoard() { return board; }
 }

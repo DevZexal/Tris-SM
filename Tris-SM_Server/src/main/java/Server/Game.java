55e6016508c421ebec1e1cf505984dd5d241a76d
IlRum3no <166618039+IlRum3no@users.noreply.github.com>
2026-01-20 11:25:12 +0100

    implemented prinBoard() function
    Implementazione della funzione che stampa la tabella

--- Tris-SM_Server/src/main/java/Server/Game.java
+++ Tris-SM_Server/src/main/java/Server/Game.java
@@ -47,7 +47,12 @@ public class Game {
     }
     
     public void printBoard() {
-    	
+    	for(int i = 0; i < 3; i++) {
+    		for (int j = 0; j < 3; j++){
+    			System.out.print(board[i][j]+ " ");
+    		}
+    		System.out.println();
+    	}
     }
     
     

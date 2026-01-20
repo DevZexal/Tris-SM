47777420c2430dd3e0ccbda112b3a08f8fcf5420
IlRum3no <166618039+IlRum3no@users.noreply.github.com>
2026-01-20 11:21:42 +0100

    Implemented isBoardFull() function
    Implementazione della funzione che controlla se la matrice è piena o non

--- Tris-SM_Server/src/main/java/Server/Game.java
+++ Tris-SM_Server/src/main/java/Server/Game.java
@@ -35,7 +35,15 @@ public class Game {
     }
 
     private boolean isBoardFull() {
-        
+ 
+        for (int i = 0; i < 3; i++) {
+        	for (int j = 0; j < 3; j++) {
+        		if (board[i][j]== '-') {
+        			return false;
+        		}
+        	}
+        }
+        return true;
     }
     
     public void printBoard() {

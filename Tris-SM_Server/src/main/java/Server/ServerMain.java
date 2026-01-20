b7ea513818658be332569352cf5884a22c0bf045
Elisei <mailatelisei27@gmail.com>
2026-01-20 11:47:31 +0100

    Implemented ServerMain

--- Tris-SM_Server/src/main/java/Server/ServerMain.java
+++ Tris-SM_Server/src/main/java/Server/ServerMain.java
@@ -11,16 +11,22 @@ public class ServerMain {
 		final int PORT = 5500;
 
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-
-            System.out.println("Server avviato sulla porta " + PORT);
+        	
+            ListaClient listaClient = new ListaClient();        
+            System.out.println("Server Tris-SM Aperto sulla porta " + PORT);
 
             while (true) {
             	
+            	Socket nuovoClient = serverSocket.accept();
+            	listaClient.addClient(nuovoClient);
+            	
+            	Thread connessioneThread = new Thread(new ThreadConnessione(nuovoClient, listaClient));
+                connessioneThread.start();
                 
             }
 
         } catch (IOException e) {
-            System.out.println("Errore server");
+            System.out.println("Errore di connessione");
         }
 	}
 

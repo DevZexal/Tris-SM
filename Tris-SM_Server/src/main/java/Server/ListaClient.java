b9f207d65d97f751596440d4f4dba2932198d9cd
Elisei <mailatelisei27@gmail.com>
2026-01-27 10:54:57 +0100

    Added ListaClient.java

--- Tris-SM_Server/src/main/java/Server/ListaClient.java
+++ Tris-SM_Server/src/main/java/Server/ListaClient.java
@@ -12,12 +12,25 @@ public class ListaClient {
         listaSockets = new ArrayList<Socket>();
     }
 
-    public synchronized void addClient(Socket c) {
-        listaSockets.add(c);
+    public synchronized void addClient(Socket client) {
+        listaSockets.add(client);
     }
 
     // Metodo che combina i player dalla lista dei client
     public synchronized void cercaPartita(ThreadConnessione attuale) {
-        
+    	if (playerWaiting == null) {
+            playerWaiting = attuale;
+            attuale.inviaMessaggio("In attesa di un altro giocatore...");
+        } else {
+            // Trovato avversario: crea il Game e accoppiali
+            Game nuovaPartita = new Game();
+            ThreadConnessione p1 = playerWaiting;
+            ThreadConnessione p2 = attuale;
+
+            p1.setAvversario(p2, nuovaPartita, 'X');
+            p2.setAvversario(p1, nuovaPartita, 'O');
+
+            playerWaiting = null; // Svuota la sala d'attesa
+        }
     }
 }
